a04091144dc857b71a7b8aaf430ae5d7a43cf5c1
jiwoong-jung <jjw981119@naver.com>
2024-11-14 15:40:26 +0900

    1114

--- sb1114/src/main/java/edu/du/sb1114/MyRestController.java
+++ sb1114/src/main/java/edu/du/sb1114/MyRestController.java
@@ -1,6 +1,7 @@
 package edu.du.sb1114;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -19,9 +20,15 @@ public class MyRestController {
         return name;
     }
 
+//    @PostMapping("/account")
+//    public Account account(@RequestBody Account account) {
+//        log.info(account.toString());
+//        return account;
+//    }
+
     @PostMapping("/account")
-    public String account(@RequestBody Account account) {
+    public ResponseEntity<?> account(@RequestBody Account account) {
         log.info(account.toString());
-        return account.toString();
+        return ResponseEntity.ok(account);
     }
 }
